c6a9d64c0689f8dd017593205c5251529d310814
fatima-elfat <ffzelfatehy@gmail.com>
2024-05-15 21:20:20 +0100

    Add files of task: 2

---
+++ controllers/AppController.js
@@ -0,0 +1,42 @@
+/**
+ * Task 2. First API.
+ * contains the definition of the 2 endpoints.
+ */
+
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+class AppController {
+  /**
+   * GET /status should return if Redis is alive and if the
+   * DB is alive too by using the 2 utils created previously:
+   * { "redis": true, "db": true } with a status code 200.
+   * @param {request} request
+   * @param {response} response
+   */
+  static getStatus(request, response) {
+    const status = {
+      db: dbClient.isAlive(),
+      redis: redisClient.isAlive(),
+    };
+    response.status(200).send(status);
+  }
+
+  /**
+   * GET /stats should return the number of users and files in DB:
+   * { "users": 12, "files": 1231 } with a status code 200
+   * @param {request} request
+   * @param {response} response
+   */
+  static async getStats(request, response) {
+    // users collection must be used for counting all users
+    // files collection must be used for counting all files
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
+  
+export default AppController;
+  
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,22 @@ function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
   /**
+   * Task 2. First API.
+   * GET /status => AppController.getStatus
+   */
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  /**
+   * Task 2. First API.
+   * GET /stats => AppController.getStats
+   */
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+
+  /**
    * Task 3. Create a new user.
    * POST /users => UsersController.postNew
    */
