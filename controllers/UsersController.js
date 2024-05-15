c1055876db912ea3c94a4fa2f620c6c15a56e31c
fatima-elfat <ffzelfatehy@gmail.com>
2024-05-15 21:27:26 +0100

    fix errors: lint 07

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,25 +2,27 @@
  * Task 3. Create a new user.
  */
 
-import dbClient from '../utils/db';
 import Queue from 'bull';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 const userQueue = new Queue('userQueue');
 
 class UsersController {
   static async addUser(req, resp){
     const { email, password } = req.body;
-  
-    if (!email)
+    if (!email){
       return resp.status(400).send({ error: 'Missing email' });
-    if (!password)
+    }
+    if (!password){
       return resp.status(400).send({ error: 'Missing password' });
+    }
     const em = await dbClient.usersCollection.findOne({ email });
     const pwd = sha1(password);
     let result;
-    if (em)
+    if (em){
       return resp.status(400).send({ error: 'Already exist' });
+    }
     try {
       result = await dbClient.usersCollection.insertOne({
       email,
@@ -35,8 +37,7 @@ class UsersController {
       userId: result.insertedId.toString(),
     });
     return resp.status(201).send(user);
-    }
+  }
 }
-
 
 export default UsersController;
